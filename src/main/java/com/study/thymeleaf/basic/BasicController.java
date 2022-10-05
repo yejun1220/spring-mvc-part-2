e132b882fb101d2225c4632037060b76b85561d0
im-yejun <yejun_1220@naver.com>
2022-10-05 22:25:15 +0900

    Expression basic objects

--- src/main/java/com/study/thymeleaf/basic/BasicController.java
+++ src/main/java/com/study/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package com.study.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -44,6 +47,20 @@ public class BasicController {
         model.addAttribute("users", list);
         model.addAttribute("userMap", map);
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session, HttpServletRequest request, Model model) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        return "basic/basic-obejcts";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
     }
 
     @Data
