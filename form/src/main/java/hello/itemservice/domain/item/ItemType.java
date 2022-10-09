55d0a48a7790ac87ae7ee486a4778af4eeb15afe
im-yejun <yejun_1220@naver.com>
2022-10-10 02:30:00 +0900

    Add itemType(enum0) and deliveryCode

---
+++ form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,16 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+
+@Getter
+public enum ItemType {
+
+    BOOK("도서"), FOOD("식품"), ETC("기타");
+
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+
+}
