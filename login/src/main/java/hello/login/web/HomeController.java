bd678c4876e306c273093062fed5884e33d55af0
im-yejun <yejun_1220@naver.com>
2022-10-30 05:25:48 +0900

    Handling Login - Servlet HTTP Session1

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -42,7 +43,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping
+    //    @GetMapping
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 회원
@@ -54,6 +55,26 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+        // 로그인 하기 전에 session을 생성하면 안되므로 false로 해야 한다.
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home으로 이동한다.
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션에 회원 데이터가 있으면 loginHome으로 이동한다.
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,6 @@
+package hello.login.web;
+
+public abstract class SessionConst {
+    // 상수처리는 abstract나 interface가 좋다.
+    public static final String LOGIN_MEMBER = "loginMember";
+}
